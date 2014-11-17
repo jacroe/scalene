fc25a44839c26dc4de62abcc3b69cdc572fe4ff9
Jacob Roeland <jacob@jacroe.com>
2014-11-16 22:51:36 -0500

    Updated the pushover library to use links

--- scalene/lib/Pushover_lib.php
+++ scalene/lib/Pushover_lib.php
@@ -12,18 +12,25 @@ class Pushover extends Library
 			$this->{$var} = $value;
 	}
 
-	public function send($title, $message, $pri=0)
+	public function send($title, $message, $url=null, $pri=0)
 	{
 		if ($pri > 1) $pri = 1;
-		curl_setopt_array($ch = curl_init(), array(
-		CURLOPT_URL => "https://api.pushover.net/1/messages.json",
-		CURLOPT_RETURNTRANSFER => 1,
-		CURLOPT_POSTFIELDS => array(
-		  "token" => $this->appID,
-		  "user" => $this->userID,
-		  "title" => $title,
-		  "message" => $message,
-		  "priority" => $pri )));
+		curl_setopt_array(
+			$ch = curl_init(),
+			array(
+				CURLOPT_URL => "https://api.pushover.net/1/messages.json",
+				CURLOPT_RETURNTRANSFER => 1,
+				CURLOPT_POSTFIELDS => array(
+					"token" => $this->appID,
+					"user" => $this->userID,
+					"title" => $title,
+					"message" => $message,
+					"priority" => $pri,
+					"url" => is_array($url) ? $url["link"] : $url,
+					"url_title" => is_array($url) ? $url["title"] : null,
+				)
+			)
+		);
 		$message = curl_exec($ch);
 		curl_close($ch);
 		if (json_decode($message)->status) return true;
