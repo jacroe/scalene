a25ba7f29f0c63d9e5a0f63c51c10d48d4a10da4
Jacob Roeland <jacob@jacroe.com>
2022-09-05 22:29:40 -0400

    Add Oxford comma method to strings helper

--- scalene/helpers/strings_helper.php
+++ scalene/helpers/strings_helper.php
@@ -18,3 +18,35 @@ if (!function_exists("camelize"))
 		return strtolower($str[0]).substr(str_replace(' ', '', ucwords(preg_replace('/[\s_]+/', ' ', $str))), 1);
 	}
 }
+
+// https://stackoverflow.com/a/9074663
+if (!function_exists("oxford_comma"))
+{
+	function oxford_comma(array $array, $conjunction = null)
+	{
+		if (is_null($conjunction)) {
+			return implode(', ', $array);
+		}
+
+		$arrayCount = count($array);
+
+		switch ($arrayCount) {
+
+			case 1:
+				return $array[0];
+				break;
+
+			case 2:
+				return $array[0] . ' ' . $conjunction . ' ' . $array[1];
+		}
+
+		// 0-index array, so minus one from count to access the
+		//  last element of the array directly, and prepend with
+		//  conjunction
+		$array[($arrayCount - 1)] = $conjunction . ' ' . end($array);
+
+		// Now we can let implode naturally wrap elements with ','
+		//  Space is important after the comma, so the list isn't scrunched up
+		return implode(', ', $array);
+	}
+}
